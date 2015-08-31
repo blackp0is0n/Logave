aa38c73e3c56cf150bd96f981eda0b2fd2653f25
Alex <alex@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2015-08-31 14:54:26 +0300

    New

--- Logave/ViewController.swift
+++ Logave/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println(self.sayhello("Andryuha!"))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func sayhello(myName: String) -> String {
+        println("Hello,"+myName)
+        
+        return "Die motherfucker"
+    }
 
 }
 
