5d639c49c844d9a332b5419ef53308d3d00830e6
Andrew Johnsson <androID969@me.com>
2015-09-13 14:54:34 +0300

    Minor Changes

---
+++ Logave/MasterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.swift
+//  Logave
+//
+//  Created by Andrew Johnsson on 13.09.15.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController:UITableViewController{
+    
+}
---
+++ Logave/SideViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SideViewController.swift
+//  Logave
+//
+//  Created by Andrew Johnsson on 13.09.15.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+import UIKit
+
+class SideViewController: UITableViewController{
+
+    @IBAction func showOrdersView(sender: AnyObject) {
+        //MasterViewController.performSegueWithIdentifier("showOrders", sender: storyboard.)
+    }
+    
+    @IBAction func showMessagesView(sender: AnyObject) {
+       // MasterViewController.performSegueWithIdentifier("showMessages", sender: self)
+    }
+}
