10f9cfdc26019d138f842e4765fcb61fa9a20c87
Andrew Johnsson <androID969@me.com>
2015-09-17 02:25:13 +0300

    Map Added

---
+++ Logave/MapController.swift
@@ -0,0 +1,14 @@
+//
+//  MapController.swift
+//  Logave
+//
+//  Created by Andrew Johnsson on 16.09.15.
+//  Copyright © 2015 Logave. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapController:UIViewController{
+    @IBOutlet weak var mapView: MKMapView!
+}
