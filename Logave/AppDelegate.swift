bacbc74e09e1efac8ada0a1c06947009ac4535ae
Andrey Ivanov <andrewjohnsson@icloud.com>
2015-11-18 19:49:51 +0300

    Minor Changes

--- Logave/AppDelegate.swift
+++ Logave/AppDelegate.swift
@@ -35,6 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
+        print("CFuck0ff")
         self.saveContext()
     }
 
