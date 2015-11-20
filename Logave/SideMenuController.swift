9d4646f72047aaae431faf022317db3ddb43c58c
Sn0w <alexandrsivakov@me.com>
2015-11-20 18:34:55 +0300

    small Updates

--- Logave/SideMenuController.swift
+++ Logave/SideMenuController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class SideMenuController: UITableViewController{
-    
     let menuItems = ["Tasks","Messages","Settings"]
     
+    var tasksController:UINavigationController? = nil
+    var messagesController:UINavigationController? = nil
+    var slidecontroller:SlideMenuController? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return menuItems.count
     }
@@ -21,8 +27,20 @@ class SideMenuController: UITableViewController{
         let cell = self.tableView.dequeueReusableCellWithIdentifier("optionCell", forIndexPath: indexPath) as UITableViewCell!
         
         cell.textLabel!.text = menuItems[indexPath.row]
-        
+        //self.slideMenuController()?.changeMainViewController(self.mainViewController, close: true)
         return cell
+    }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        switch indexPath.row {
+        case 0:
+            print("0 is selected")
+            self.slidecontroller?.changeMainViewController(tasksController!, close: true)
+        case 1:
+            print("1 selected")
+            self.slidecontroller?.changeMainViewController(messagesController!, close: true)
+        default:
+            print("Default")
+        }
     }
     
 }
