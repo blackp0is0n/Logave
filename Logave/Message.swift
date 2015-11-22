cba009daab4505b5fdb2f468499921b46c857aba
Sn0w <alexandrsivakov@me.com>
2015-11-22 17:33:57 +0300

    New structure of message entity

--- Logave/Message.swift
+++ Logave/Message.swift
@@ -9,5 +9,11 @@
 import Foundation
 
 class Message{
+    var id:Int = 0
+    var date:NSDate = NSDate()
     
+    var topic:String = ""
+    var message:String = ""
+    var name:String = ""
+    var sname:String = ""
 }
